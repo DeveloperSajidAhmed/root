9011be7ee9431e759325f716ed9451076ff19855
DeveloperSajidAhmed <developersajidahmed@gmail.com>
2024-05-13 14:26:49 +0600

    first commit

---
+++ run.js
@@ -0,0 +1,19 @@
+var a;
+a =prompt("type your value A");
+var b;
+b =prompt("type your value B");
+
+// a+b =  6
+// a*b =  8
+
+// a = 2a-a = (a+b)+(a-b)
+if(a + b ==6 && a*b ==8) {
+    document.write ("Number of a is :",a + "<br>");
+    document.write ("Number of b is :",b);
+}
+// else if (a*b ==8) {
+//     document.write ("Number of a is :",a);
+// }
+else{
+    document.write ("Number nai");
+}
